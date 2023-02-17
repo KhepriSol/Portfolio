53a595fe0c8b7983851725f34bcc2bc4ff4b1c29
KhepriSol <118213369+KhepriSol@users.noreply.github.com>
2023-02-16 18:46:36 -0600

    all of it

--- assets/dist/index.dev.js
+++ assets/dist/index.dev.js
@@ -5,5 +5,13 @@ document.addEventListener("mousemove", function (e) {
   var x = e.clientX / window.innerWidth;
   var y = e.clientY / window.innerHeight;
   card.style.transform = "translate(-" + x * 20 + "px, -" + y * 20 + "px)";
-});
+}); //make it wiggle like a worm.//
+// const card = document.querySelector(".card");
+// const mainContent = document.querySelector(".main-content");
+// document.addEventListener("mousemove", function(e) {
+//   let x = e.clientX / window.innerWidth;
+//   let y = e.clientY / window.innerHeight;
+//   mainContent.style.transform = "translateX(-" + x * 10 + "px)";
+//   card.style.transform = "translateX(" + x * 20 + "px) rotateY(" + y * 20 + "deg)";
+// });
 //# sourceMappingURL=index.dev.js.map
--- assets/index.js
+++ assets/index.js
@@ -5,4 +5,16 @@ document.addEventListener("mousemove", function(e) {
   let y = e.clientY / window.innerHeight;
 
   card.style.transform = "translate(-" + x * 20 + "px, -" + y * 20 + "px)";
-});
+ });
+
+//make it wiggle like a worm.//
+// const card = document.querySelector(".card");
+// const mainContent = document.querySelector(".main-content");
+
+// document.addEventListener("mousemove", function(e) {
+//   let x = e.clientX / window.innerWidth;
+//   let y = e.clientY / window.innerHeight;
+
+//   mainContent.style.transform = "translateX(-" + x * 10 + "px)";
+//   card.style.transform = "translateX(" + x * 20 + "px) rotateY(" + y * 20 + "deg)";
+// });
